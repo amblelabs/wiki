ec44947767665df00b783b5829a28d9e92e83bc1
Theo <git@theo.is-a.dev>
2026-04-11 22:06:08 +0300

    fix: use proper search base path

--- components/search.tsx
+++ components/search.tsx
@@ -27,6 +27,7 @@ export default function DefaultSearchDialog(props: SharedProps) {
   const { locale } = useI18n(); // (optional) for i18n
   const { search, setSearch, query } = useDocsSearch({
     type: "static",
+    from: "/wiki/api/search",
     initOrama,
     locale,
   });
